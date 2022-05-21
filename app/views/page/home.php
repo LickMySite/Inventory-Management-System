790253b7db59733d0cece29383f963ddb04e6291
evan <lickmysite@gmail.com>
2022-05-21 07:47:03 -0700

    base BS navbar. Misc additions and edits

--- app/views/page/home.php
+++ app/views/page/home.php
@@ -1,6 +1,13 @@
+<div class="row">
+  <div class="col">
+  <img src="<?=ASSETS;?>img/mc5.jpg" class="img-fluid w-100" alt="...">
+  </div>
+</div>
+
+  
   <div class="container">
-    <div class="col">
-      <div class="row text-center">
+    <div class="row">
+      <div class="col text-center">
         <h1>Home Page</h1>
         <p>Welcome</p>
       </div>
