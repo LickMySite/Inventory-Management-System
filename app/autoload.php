c4b8bfc94afcfd2bab8e9abd520b91342083e740
evan <lickmysite@gmail.com>
2022-05-24 05:17:17 -0700

    Added setting class to autoload

--- app/autoload.php
+++ app/autoload.php
@@ -3,5 +3,6 @@
 	require_once "../app/core/functions.php";
 	require_once "../app/core/database.php";
 	require_once "../app/core/controller.php";
+	require_once "../app/core/setting.class.php";
 	require_once "../app/core/app.php";
 	$app = new App();
